ad0b38ea63d84f14ae438602ca02a1df361d03d2
Tom Pillokat <tom.pillokat@googlemail.com>
2020-12-08 18:12:07 +0100

    First Init

---
+++ web-socket-wrapper.ts
@@ -0,0 +1,25 @@
+export class WebSocketWrapper{
+    public ws: WebSocket
+    callbacks: any
+    constructor(ws: WebSocket){
+        this.ws = ws;
+        this.callbacks = {};
+        ws.onmessage = (payload: any) => {
+            console.log(payload)
+            const parsedPayload = JSON.parse(payload.data)
+            this.callbacks[parsedPayload.eventName](parsedPayload.data)
+        }
+    }
+
+    public on(eventName: string, callback: any){
+        this.callbacks[eventName] = callback
+    }
+
+    public send(eventName: string, data: any){
+        const payload = {
+            eventName: eventName,
+            data: data
+        }
+        this.ws.send(JSON.stringify(payload))
+    }
+}
